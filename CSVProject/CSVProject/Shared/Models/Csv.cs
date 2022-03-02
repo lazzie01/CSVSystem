69cc1fdbf68e483ecf2d105020148ec643a45319
lazarusmunetsi <munetsilazzie@gmail.com>
2022-03-02 23:05:40 +0200

    added Entity models which will be mapped to database tables

---
+++ CSVProject/CSVProject/Shared/Models/Csv.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSVProject.Shared.Models
+{
+    public class Csv
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string FilePath { get; set; }
+    }
+}
