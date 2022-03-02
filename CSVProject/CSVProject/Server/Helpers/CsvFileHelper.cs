afceef8c4025764c5beeea533f909a9536ac4748
lazarusmunetsi <munetsilazzie@gmail.com>
2022-03-02 23:09:06 +0200

    added Helpers folder which will contain all project helper classes

---
+++ CSVProject/CSVProject/Server/Helpers/CsvFileHelper.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CSVProject.Server.Helpers
+{
+    public static class CsvFileHelper
+    {
+        public static readonly CsvConfiguration defaultCsvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";",
+            HasHeaderRecord = true
+        };
+
+        public static IEnumerable<T> ReadFile<T>(string csvFilePath, CsvConfiguration csvConfiguration = null)
+        {
+            csvConfiguration = csvConfiguration ?? defaultCsvConfiguration;
+            using var reader = new StreamReader(csvFilePath);
+            using var csv = new CsvReader(reader, csvConfiguration);
+            return csv.GetRecords<T>();
+        }
+
+        public static void UpdateFile<T>(string csvFilePath, IEnumerable<T> dataToWrite, bool truncateFileBeforeWriting = false, CsvConfiguration csvConfiguration = null)
+        {
+            csvConfiguration = csvConfiguration ?? defaultCsvConfiguration;
+
+            if (truncateFileBeforeWriting)
+            {
+                File.WriteAllText(csvFilePath, string.Empty);
+            }
+
+            using (var writer = new StreamWriter(csvFilePath))
+            using (var csvWriter = new CsvWriter(writer, csvConfiguration))
+            {
+                csvWriter.WriteHeader<T>();
+                csvWriter.WriteRecords(dataToWrite);
+                writer.Flush();
+            }
+        }
+
+    }
+}
