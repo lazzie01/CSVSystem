0541a26c2bb0616440c2b3d2fa634b7e1c480585
lazarusmunetsi <munetsilazzie@gmail.com>
2022-03-03 09:19:14 +0200

    set the Id primary key to auto increment

--- CSVProject/CSVProject/Shared/Models/Csv.cs
+++ CSVProject/CSVProject/Shared/Models/Csv.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CSVProject.Shared.Models
 {
     public class Csv
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
